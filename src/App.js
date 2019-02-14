170c2cca3bed108a8352246b61dad2a66bd17e4a
Julio Henderson <julio.henderson98@gmail.com>
2019-02-14 09:56:05 -0300

    feat(App): add components and request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import axios from 'axios';
+
+//components
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import Loading from './components/Loading';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loadging: false
+  }
+  componentWillMount(){
+    axios
+    .get('./assets/feed.json')
+    .then(res => {
+      this.setState({loading: true})
+
+      return setTimeout(() => {
+        this.setState({users: res.data, loading: false})
+      }, 1500); 
+    })
+    .catch(err => console.log(err))
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        {this.state.loading ? <Loading color="#E51C72"/> :
+          <Fragment>
+            {this.state.users.map( user => <ListItem name={user.name} thumbnail={user.image} key={parseInt(Math.random()*1000)} />)}
+          </Fragment>
+        } 
       </div>
     );
   }
